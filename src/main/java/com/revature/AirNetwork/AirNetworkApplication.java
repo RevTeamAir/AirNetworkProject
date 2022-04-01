3e2293371c9622f8af51769f9395b8f9fa33300f
onivose <oliviernivose@gmail.com>
2022-04-01 15:32:53 -0400

    first commit

---
+++ src/main/java/com/revature/AirNetwork/AirNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.revature.AirNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/AirNetwork/AirNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.AirNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
